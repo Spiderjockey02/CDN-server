8980126a9ab384b4e5cc66379c207fb6a96170f0
Spiderjockey02 <benjamin.forey11@gmail.com>
2024-12-28 15:52:39 +0000

    Turned the directory rows into its own component

--- website/src/components/directory.tsx
+++ website/src/components/directory.tsx
@@ -1,27 +1,30 @@
 import type { fileItem } from '../utils/types';
-import { formatBytes, getFileIcon } from '../utils/functions';
-import Link from 'next/link';
 import { useState } from 'react';
 import ContextMenu from '../components/menus/contextMenu';
 import type { MouseEvent } from 'react';
+import FileItemRow from './views/FileItemRow';
 type sortKeyTypes = 'Name' | 'Size' | 'Date_Mod';
 type SortOrder = 'ascn' | 'dscn';
 interface Props {
   files: fileItem
   dir: string
+	userId: string
 }
 
 const initalContextMenu = {
 	show: false,
 	x: 0,
 	y: 0,
-	selected: '',
+	selected: { name: '', type: '', size: 0, modified: 0, path: '' },
 };
 
-export default function Directory({ files, dir }: Props) {
+export default function Directory({ files, dir, userId }: Props) {
 	const [sortKey, setSortKey] = useState<sortKeyTypes>('Name');
 	const [sortOrder, setSortOrder] = useState<SortOrder>('ascn');
 	const [contextMenu, setContextMenu] = useState(initalContextMenu);
+	const [filesSelected, setFilesSelected] = useState<string[]>([]);
+	const [allSelected, setAllSelected] = useState(false);
+
 
 	function updateSortKey(sort: sortKeyTypes) {
 		switch(sort) {
@@ -58,13 +61,33 @@ export default function Directory({ files, dir }: Props) {
 		}
 	}
 
-	function openContextMenu(e: MouseEvent<HTMLTableRowElement>, selected: string) {
+	function openContextMenu(e: MouseEvent<HTMLTableRowElement>, selected: fileItem) {
 		e.preventDefault();
 		const { pageX, pageY } = e;
 		setContextMenu({ show: true, x: pageX, y: pageY, selected });
 	}
-
 	const closeContextMenu = () => setContextMenu(initalContextMenu);
+
+	function handleCheckboxToggle(e: MouseEvent, fileName: string) {
+		e.stopPropagation();
+		setFilesSelected((prevSelected) =>
+			prevSelected.includes(fileName)
+				? prevSelected.filter((name) => name !== fileName)
+				: [...prevSelected, fileName],
+		);
+	}
+
+	function handleSelectAllToggle() {
+		if (allSelected) {
+			// Uncheck all
+			setFilesSelected([]);
+		} else {
+			// Select all files
+			const allFileNames = files.children.map((file) => file.name);
+			setFilesSelected(allFileNames);
+		}
+		setAllSelected(!allSelected);
+	}
 
 	return (
 		<div>
@@ -74,7 +97,7 @@ export default function Directory({ files, dir }: Props) {
 					<tr>
 						<th scope="col" className="th-header dot" style={{ width:'5%', textAlign:'center', borderTopLeftRadius: '5px' }}>
 							<div className="form-check form-check-inline hide">
-								<input className="form-check-input" type="checkbox" name="exampleRadios" id="All" data-option="0" />
+								<input className="form-check-input" type="checkbox" name="exampleRadios" id="All" onChange={handleSelectAllToggle} />
 							</div>
 						</th>
 						<th id="Type" className="th-header" scope="col" style={{ width:'5%', textAlign:'center' }}>
@@ -93,51 +116,23 @@ export default function Directory({ files, dir }: Props) {
 				</thead>
 				<tbody>
 					{files.children.filter(f => f.type == 'directory').map(_ => (
-						<tr key={_.name} onContextMenu={(e) => openContextMenu(e, _.name)}>
-							<th className="dot" style={{ textAlign:'center' }}>
-								<div className="form-check form-check-inline hide">
-									<input className="form-check-input" type="checkbox" name="exampleRadios" data-option="0" />
-								</div>
-							</th>
-							<th id="Type" scope="col" style={{ textAlign:'center' }} dangerouslySetInnerHTML={{ __html: getFileIcon(_) }}></th>
-							<th scope="row" className="text-truncate" style={{ maxWidth: 'calc( 70 * 1vw )' }}>
-								<Link href={`/files${dir == '/' ? '/' : `/${dir}/`}${_.name}`}>{_.name}</Link>
-							</th>
-							<td>{_.children?.length ?? 0} file{_.children?.length !== 1 ? 's' : ''}</td>
-							<td>{new Date(_.modified).toLocaleString('en-US')}</td>
-						</tr>
+						<FileItemRow key={_.name}
+							dir={dir} file={_}
+							userId={userId}
+							isChecked={filesSelected.includes(_.name)} openContextMenu={openContextMenu}
+							handleCheckboxToggle={handleCheckboxToggle}
+						/>
 					))}
-
 					{files.children.filter(f => f.type == 'file').map(_ => (
-						<tr key={_.name} onContextMenu={(e) => openContextMenu(e, _.name)}>
-							<th className="dot" style={{ textAlign:'center' }}>
-								<div className="form-check form-check-inline hide">
-									<input className="form-check-input" type="checkbox" name="exampleRadios" id={encodeURI(_.name)} data-option="0" />
-								</div>
-							</th>
-							<th id="Type" scope="col" style={{ textAlign:'center' }} dangerouslySetInnerHTML={{ __html: getFileIcon(_) }}></th>
-							<th scope="row" className="text-truncate" style={{ maxWidth: 'calc( 70 * 1vw )' }}>
-								<Link href={`/files${dir == '/' ? '/' : `/${dir}/`}${_.name}`}>{_.name}</Link>
-							</th>
-							<td>{formatBytes(_.size)}</td>
-							<td>{new Date(_.modified).toLocaleString('en-US')}</td>
-						</tr>
+						<FileItemRow key={_.name}
+							dir={dir} file={_}
+							userId={userId}
+							isChecked={filesSelected.includes(_.name)} openContextMenu={openContextMenu}
+							handleCheckboxToggle={handleCheckboxToggle}
+						/>
 					))}
 				</tbody>
 			</table>
-			<style>
-				{`
-          .th-header:hover {
-            background-color: grey;
-            cursor: pointer;
-          }
-          tbody tr:hover {
-            border-radius: 15px;
-            cursor: pointer;
-            box-shadow: 0px 0px 1px 1px #999;
-          }
-          `}
-			</style>
 		</div>
 	);
 }
---
+++ website/src/components/views/FileItemRow.tsx
@@ -0,0 +1,58 @@
+import { getFileIcon, formatBytes } from '@/utils/functions';
+import { fileItem } from '@/utils/types';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useState, type MouseEvent } from 'react';
+import path from 'path';
+
+interface Props {
+  file: fileItem
+  dir: string
+	userId: string
+	isChecked: boolean
+	openContextMenu: (e: MouseEvent<HTMLTableRowElement>, file: fileItem) => void
+	handleCheckboxToggle: (e: MouseEvent, fileName: string) => void
+}
+
+export default function FileItemRow({ dir, userId, file, isChecked, openContextMenu, handleCheckboxToggle }: Props) {
+	const [show, setShow] = useState(false);
+	const router = useRouter();
+	const handleRowClick = () => router.push(path.join('/files', dir, file.name));
+
+	const handleTextClick = (e: MouseEvent) => {
+		e.stopPropagation();
+		setShow(true);
+	};
+	return (
+		<>
+			<tr key={file.name} style={{ cursor: 'pointer' }} onContextMenu={(e) => openContextMenu(e, file)} onClick={handleRowClick}>
+				<th className="dot" style={{ textAlign:'center' }} onClick={(e) => handleCheckboxToggle(e, file.name)}>
+					<div className="form-check form-check-inline hide">
+						<input className="form-check-input" type="checkbox" name="exampleRadios" id={encodeURI(file.name)} checked={isChecked} />
+					</div>
+				</th>
+				<th id="Type" scope="col" style={{ textAlign:'center' }} dangerouslySetInnerHTML={{ __html: getFileIcon(file) }}></th>
+				<th scope="row" className="text-truncate" style={{ maxWidth: 'calc( 70 * 1vw )' }}>
+					<Link onClick={handleTextClick} style={{ textDecoration: 'none', color: 'black' }} href="#">{file.name}</Link>
+				</th>
+				<td>{file.type == 'file' ? formatBytes(file.size) : file.children?.length ?? 0} file{file.children?.length !== 1 ? 's' : ''}</td>
+				<td>{new Date(file.modified).toLocaleString('en-US')}</td>
+			</tr>
+			<div className={`offcanvas offcanvas-end ${show ? 'show' : ''}`} id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" style={{ maxWidth: '75%' }}>
+				<div className="offcanvas-header">
+					<h5 className="offcanvas-title" id="offcanvasExampleLabel">File preview</h5>
+					<button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close" onClick={() => setShow(false)}></button>
+				</div>
+				<div className="offcanvas-body">
+					<img src={`/thumbnail/${userId}/${dir}/${file.name}`} alt={file.name} style={{ maxWidth: '100%' }} />
+					<br />
+					{file.name}
+					<br />
+					{new Date(file.modified).toLocaleString('en-US')}
+					<br />
+					{file.type == 'file' ? formatBytes(file.size) : file.children?.length ?? 0} file{file.children?.length !== 1 ? 's' : ''}
+				</div>
+			</div>
+		</>
+	);
+}
