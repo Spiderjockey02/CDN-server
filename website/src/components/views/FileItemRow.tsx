50eea6ecc9642485b38d64a80addb4246835ab43
Spiderjockey02 <benjamin.forey11@gmail.com>
2025-02-17 22:31:02 +0000

    Updated table row for displaying file information

--- website/src/components/views/FileItemRow.tsx
+++ website/src/components/views/FileItemRow.tsx
@@ -3,6 +3,7 @@ import { fileItem } from '@/types';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import type { MouseEvent }from 'react';
+import FileDetail from './FileDetail';
 
 interface Props {
   file: fileItem
@@ -10,9 +11,10 @@ interface Props {
 	openContextMenu: (e: MouseEvent<HTMLTableRowElement>, file: fileItem) => void
 	handleCheckboxToggle: (e: MouseEvent, file: fileItem) => void
 	setShow: (fileId: string) => void
+	showMoreDetail?: boolean
 }
 
-export default function FileItemRow({ file, isChecked, openContextMenu, handleCheckboxToggle, setShow }: Props) {
+export default function FileItemRow({ file, isChecked, openContextMenu, handleCheckboxToggle, setShow, showMoreDetail = false }: Props) {
 	const router = useRouter();
 	const handleRowClick = () => router.push(`/files${file.path}`);
 
@@ -24,17 +26,17 @@ export default function FileItemRow({ file, isChecked, openContextMenu, handleCh
 	return (
 		<>
 			<tr key={file.name} style={{ cursor: 'pointer' }} onContextMenu={(e) => openContextMenu(e, file)} onClick={handleRowClick}>
-				<th className="dot" style={{ textAlign:'center' }} >
-					<div className="form-check form-check-inline hide">
-						<input className="form-check-input" type="checkbox" id={encodeURI(file.name)} checked={isChecked} onClick={(e) => handleCheckboxToggle(e, file)} />
-					</div>
+				<th className='text-center hide-on-mobile'>
+					<input className="form-check-input" type="checkbox" id={encodeURI(file.name)} checked={isChecked} onClick={(e) => handleCheckboxToggle(e, file)} />
 				</th>
-				<th id="Type" scope="col" style={{ textAlign:'center' }}>{getFileIcon(file)}</th>
-				<th scope="row" className="text-truncate" style={{ maxWidth: 'calc( 70 * 1vw )' }}>
-					<Link onClick={handleTextClick} style={{ textDecoration: 'none', color: 'black' }} href="#">{file.name}</Link>
+				<th scope="row" className="text-truncate" style={{ maxWidth: '50vw' }}>
+					{showMoreDetail ?
+						<FileDetail file={file} /> :
+						<Link onClick={handleTextClick} style={{ textDecoration: 'none', color: 'black' }} href="#">{getFileIcon(file)} {file.name}</Link>
+					}
 				</th>
 				<td>{file.type == 'FILE' ? formatBytes(file.size) : `${file._count?.children ?? 0} files`}</td>
-				<td>{new Date(file.createdAt).toLocaleString('en-US')}</td>
+				<td className='hide-on-mobile'>{new Date(file.createdAt).toLocaleString('en-GB')}</td>
 			</tr>
 		</>
 	);
